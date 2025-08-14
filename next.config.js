9733a2079dfdf9ca57e7592db3aacaca3719c672
FredWav <99555684+FredWav@users.noreply.github.com>
2025-08-14 11:39:51 +0200

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['p16-sign-va.tiktokcdn.com', 'sf16-ies-music-va.tiktokcdn.com'],
+  },
+}
+
+module.exports = nextConfig
